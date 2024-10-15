b54c1e15d70894f0d2b67171c70244a99ede0c3b
andrea <andreamartinezruiz10@gmail.com>
2024-10-15 11:09:15 +0200

    primer commit

---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Cliente {
+    private int id;
+    private String deni;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+}
